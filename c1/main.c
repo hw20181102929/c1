ab2fd2ef2cf71470d1a1b5a87852a42cade30a85
hw20181102929 <51589984+hw20181102929@users.noreply.github.com>
2019-10-23 10:22:05 +0800

    Update main.c

--- c1/main.c
+++ c1/main.c
@@ -1,135 +1,18 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-typedef int ElementType;
-typedef struct TNode *Position;
-typedef Position BinTree;
-struct TNode{
-    ElementType Data;
-    BinTree Left;
-    BinTree Right;
-};
-
-void PreorderTraversal( BinTree BT ); /* 先序遍历，由裁判实现，细节不表 */
-void InorderTraversal( BinTree BT );  /* 中序遍历，由裁判实现，细节不表 */
-
-BinTree Insert( BinTree BST, ElementType X );
-BinTree Delete( BinTree BST, ElementType X );
-Position Find( BinTree BST, ElementType X );
-Position FindMin( BinTree BST );
-Position FindMax( BinTree BST );
-
-int main()
+void LevelorderTraversal( BinTree BT )
 {
-    BinTree BST, MinP, MaxP, Tmp;
-    ElementType X;
-    int N, i;
-    
-    BST = NULL;
-    scanf("%d", &N);
-    for ( i=0; i<N; i++ ) {
-        scanf("%d", &X);
-        BST = Insert(BST, X);
-    }
-    printf("Preorder:"); PreorderTraversal(BST); printf("\n");
-    MinP = FindMin(BST);
-    MaxP = FindMax(BST);
-    scanf("%d", &N);
-    for( i=0; i<N; i++ ) {
-        scanf("%d", &X);
-        Tmp = Find(BST, X);
-        if (Tmp == NULL) printf("%d is not found\n", X);
-        else {
-            printf("%d is found\n", Tmp->Data);
-            if (Tmp==MinP) printf("%d is the smallest key\n", Tmp->Data);
-            if (Tmp==MaxP) printf("%d is the largest key\n", Tmp->Data);
-        }
-    }
-    scanf("%d", &N);
-    for( i=0; i<N; i++ ) {
-        scanf("%d", &X);
-        BST = Delete(BST, X);
-    }
-    printf("Inorder:"); InorderTraversal(BST); printf("\n");
-    
-    return 0;
-}
-BinTree Insert( BinTree BST, ElementType X )
-{
-    if(!BST)
+    if(BT)
     {
-        BST=(BinTree)malloc(sizeof(struct TNode));
-        BST->Data=X;
-        BST->Left=BST->Right=NULL;
-    }
-    else
-    {
-        if(X>BST->Data)
+        BinTree p[100];
+        int front=0;
+        int rear=0;
+        p[++rear]=BT;
+        while(front!=rear)
         {
-            BST->Right=Insert(BST->Right, X);
-        }
-        else if(X<BST->Data)
-        {
-            BST->Left=Insert(BST->Righr, X);
+            BinTree p1;
+            p1=p[++front];
+            printf(" %c",p1->data);
+            if(p1->Left!=NULL)p[++rear]=p1->Left;
+            if(p1->Right!=NULL)p[++rear]=p1->Right;
         }
     }
-    return BST;
-}
-BinTree Delete( BinTree BST, ElementType X )
-{
-    if(BST)
-    {
-        if(X>BST->Data)
-        {
-            BST->Right=Delete(BST->Right,X);
-        }
-        else if(X<BST->Data)
-        {
-            BST->Left=Delete(BST->Left,X);
-        }
-        else
-        {
-            if(BST->Left&&BST->Right)
-            {
-                Bintree m;
-                m=FindMax(BST->Left);
-                BST->Data=m->Data;
-                BST->Left=Delete(BST->Left,BST->Data);
-            }
-            if(BST->Rigth)
-            {
-                BST=BST->Right;
-            }
-            else
-            {
-                BST=BST->Left;
-            }
-        }
-    }
-    else
-    {
-        printf("Not Found\n");
-    }
-    return BST;
-}
-Position Find( BinTree BST, ElementType X )
-{
-    if(BST)
-    {
-        if(X>BST->Data)
-        {
-            return Find(BST->Right, X);
-        }
-        else if(X<BST->Data)
-        {
-            return Find(BST->Left,X);
-        }
-    }
-    return BST;
-}
-Position FindMin( BinTree BST )
-{
-    if(!BST) return NULL;
-    if(BST->Left) return Find(BST->Left, X);
-    else if(!BST->Left) return BST;
 }
